38b5c908cb67f8afaa5f0f2d422590be17755e44
Kitaikuyooo <131652981+Kitaikuyooo@users.noreply.github.com>
2023-04-28 09:24:32 -0400

    index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+//資料庫資訊
+$servername = "localhost";
+$username = "sarah";
+$password = "00000";
+$dbname = "testdb";
+
+$conn = new mysqli($servername, $username, $password,$dbname);
+
+// Check確認 connection連線
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+//接著撈資料
+$sql = "SELECT * FROM Post;";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $posts=$result->fetch_all(MYSQLI_ASSOC);
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
